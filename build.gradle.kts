d7fc640e6314ad38d850d23ff2b695383e66048a
Klaus Betz <klaus.betz@deckweiss.at>
2024-06-03 16:48:43 +0200

    fix: publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val packageVersion = "2.0.0"
+
 plugins {
     id("java")
     kotlin("jvm") version "2.0.0"
@@ -9,16 +11,11 @@ plugins {
 }
 
 group = "at.deckweiss"
-version = "2.0.0"
+version = packageVersion
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
 }
 
 dependencies {
@@ -41,12 +38,39 @@ tasks.withType<KotlinCompile> {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-    signAllPublications()
+    coordinates(
+        groupId = "at.deckweiss",
+        artifactId = "logging",
+        version = packageVersion
+    )
 
     pom {
         name.set("Deckweiss Logging")
         description.set("Library for enhancing SLF4J logging by explicit parameters in the message")
+        url.set("https://git.deckweiss.at/chapters/backend/packages/logging")
         inceptionYear.set("2022")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("klausbetz")
+                name.set("Klaus Betz")
+                url.set("https://www.deckweiss.at")
+            }
+        }
+
+        scm {
+            url.set("https://git.deckweiss.at/chapters/backend/packages/logging.git")
+        }
     }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
