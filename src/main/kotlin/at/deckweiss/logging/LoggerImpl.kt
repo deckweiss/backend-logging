788d6c9b41842776aef81be0bdbfd147853de962
Klaus Betz <klaus.betz@deckweiss.at>
2024-06-14 14:42:59 +0200

    feat: replace logparams with kotlin maps

--- src/main/kotlin/at/deckweiss/logging/LoggerImpl.kt
+++ src/main/kotlin/at/deckweiss/logging/LoggerImpl.kt
@@ -1,33 +1,32 @@
 package at.deckweiss.logging
 
+import org.slf4j.spi.LoggingEventBuilder
+
 internal class LoggerImpl(private val slf4jLogger: org.slf4j.Logger) : Logger {
-    override fun debug(message: String, throwable: Throwable?, params: LogParams?) {
-        log(slf4jLogger::debug, message, throwable, params)
-    }
+  override fun trace(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    slf4jLogger.atTrace().logStructuredMessage(message, throwable, params)
+  }
 
-    override fun info(message: String, throwable: Throwable?, params: LogParams?) {
-        log(slf4jLogger::info, message, throwable, params)
-    }
+  override fun debug(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    slf4jLogger.atDebug().logStructuredMessage(message, throwable, params)
+  }
 
-    override fun warn(message: String, throwable: Throwable?, params: LogParams?) {
-        log(slf4jLogger::warn, message, throwable, params)
-    }
+  override fun info(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    slf4jLogger.atInfo().logStructuredMessage(message, throwable, params)
+  }
 
-    override fun error(message: String, throwable: Throwable?, params: LogParams?) {
-        log(slf4jLogger::error, message, throwable, params)
-    }
+  override fun warn(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    slf4jLogger.atWarn().logStructuredMessage(message, throwable, params)
+  }
 
+  override fun error(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    slf4jLogger.atError().logStructuredMessage(message, throwable, params)
+  }
 
-    private fun log(logFunc: (message: String, throwable: Throwable?) -> Unit, message: String, throwable: Throwable?, params: LogParams?) {
-        val msg = buildMessage(message, params)
-        logFunc(msg, throwable)
-    }
-
-    private fun buildMessage(message: String, params: LogParams?): String {
-        return when (params) {
-            null -> message
-            else -> "$message ${params.toParamString()}"
-        }
-    }
-
+  private fun LoggingEventBuilder.logStructuredMessage(message: String, throwable: Throwable?, params: Map<String, Any?>?) {
+    val paramsString = if (params.isNullOrEmpty()) "" else params.map { "${it.key}=\"${it.value}\"" }.joinToString(prefix = " [", postfix = "]")
+    setMessage(message + paramsString)
+    setCause(throwable)
+    log()
+  }
 }
